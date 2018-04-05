bf85a59131eed6f62f2d4ee9aec5a93bffffdd4c
Hannah Crocker <hannahcrocker@Hannahs-MacBook-Air.local>
2018-04-05 15:21:43 +0800

    first commit

---
+++ src/main/java/controller/GreetingController.java
@@ -0,0 +1,29 @@
+package controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+    @GetMapping("/quote")
+    public String quote(@RequestParam(name="name", required=false, defaultValue="Dr. Steve Brule") String name, Model model) {
+        model.addAttribute("name", name);
+        return "quote";
+    }
+}
